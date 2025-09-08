90f83a6e335bb60f95e07a2ddbe953741425b8d2
houssamyaacoub <146137872+houssamyaacoub@users.noreply.github.com>
2025-09-08 12:47:47 -0400

    Update Main.java

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args){
         FizzBuzz.newMain();
         System.out.println();
+        System.out.println();
         FizzBuzz.doFizzBuzz(6);
     }
 }
