d71994b22a6bd2f96440c05cccb8dbd4df06cbf3
houssamyaacoub <houssamyaacoub2@gmail.com>
2025-09-08 12:45:35 -0400

    New Main class

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,6 +7,10 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
+        newMain();
+    }
+
+    public static void newMain() {
         int i = 1;
         while (i < 100) {
 
@@ -14,7 +18,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
